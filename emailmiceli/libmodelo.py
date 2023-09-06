f14da8e786868532554d2cd319c7645eba58c0d2
pedro <p18062004@gmail.com>
2023-09-06 20:49:36 -0300

    Txt separados por carta

--- emailmiceli/libmodelo.py
+++ emailmiceli/libmodelo.py
@@ -1,5 +1,7 @@
+import datetime
+
 arqlog = open("login.txt", "r")
-arqcart = open("cartas.txt", "a")
+#arqcart = open("cartas.txt", "a")
 arqsignup = open("login.txt", "a")
 
 def check(n, s):
@@ -17,7 +19,10 @@ def check(n, s):
 
 def cartinha(data, dest, msg, rem):
     
+    horario = datetime.datetime.now()
+    arqcart = open(str(horario)+".txt", "w+")
     arqcart.write(data+"\n"+dest+"\n"+msg+"\n"+rem)
+    arqcart.close()
 
 def signup(nome, senha):
      
